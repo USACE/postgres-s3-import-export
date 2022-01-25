c1394422301a44e43c10a2c291eac60d4f152144
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-01-25 15:44:06 -0500

    notes and disable-triggers

--- app/import.sh
+++ app/import.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Note - each table you restore to should be empty.  Otherwise you will probably
+# run into "duplicate key value violates unique constraint" which will stop the import
+# for that table
+
 today=`date +"%Y-%m-%d"`
 S3_PATH=${AWS_S3_BUCKET}/${S3_DB_EXPORT_DIR}/${today}
 
@@ -8,7 +12,7 @@ printf "\n1) Copying export from $S3_PATH/export.dump to local storage (import.d
 aws --endpoint-url $AWS_S3_ENDPOINT s3 cp s3://$S3_PATH/export.dump /app/data/import.dump
 
 printf "\n2) Running pg_restore on import.dump.\n\n"
-pg_restore --clean --dbname=$PGDATABASE -h $PGHOST --verbose --format=c --jobs=1 --username $PGUSER --schema $SCHEMA  < /app/data/import.dump > /app/data/import.log 2>&1
+pg_restore --clean --dbname=$PGDATABASE -h $PGHOST --verbose --format=c --jobs=1 --username $PGUSER --schema $SCHEMA --disable-triggers  < /app/data/import.dump > /app/data/import.log 2>&1
 
 aws --endpoint-url $AWS_S3_ENDPOINT s3 cp /app/data/import.log s3://$S3_PATH/import.log
 
